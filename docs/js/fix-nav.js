229977d742ceabea8f2b14543eb31b4a9cb1d11c
chanyou0311 <chanyou0311@gmail.com>
2019-01-12 19:05:17 +0900

    [add] supporters page

---
+++ docs/js/fix-nav.js
@@ -0,0 +1,10 @@
+(function($) {
+	"use strict"
+
+	// Fixed nav
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+		wScroll > 50 ? $('#header').addClass('fixed-navbar') : $('#header').removeClass('fixed-navbar');
+	});
+
+})(jQuery);
